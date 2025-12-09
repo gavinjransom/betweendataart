ecb5385edb42e8cb0c0cdd1bf33a349dcab71b66
Gavin (Annalect) <your.work@email.com>
2025-12-09 22:31:28 +0000

    Deleted duplicate pbix folder

--- projects/steps/pbix/powerquery/dim_date.m
+++
@@ -1,6 +0,0 @@
-let
-    Source = Sql.Databases("between-data-art.database.windows.net"),
-    #"Between-Data-Art-db" = Source{[Name="Between-Data-Art-db"]}[Data],
-    dbo_dim_date = #"Between-Data-Art-db"{[Schema="dbo",Item="dim_date"]}[Data]
-in
-    dbo_dim_date
