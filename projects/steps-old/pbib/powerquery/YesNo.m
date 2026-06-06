6005ac6f6d1c7ec8447bbd4834a3ee341b66ce4a
Gavin Ransom <your-email@example.com>
2026-06-06 10:29:54 +0100

    Deleted 'Steps-old' folder

--- projects/steps-old/pbib/powerquery/YesNo.m
+++
@@ -1,5 +0,0 @@
-let
-    Source = Table.FromRows(Json.Document(Binary.Decompress(Binary.FromText("i45WikwtVtJRMlSK1YlW8ssHMo2UYmMB", BinaryEncoding.Base64), Compression.Deflate)), let _t = ((type nullable text) meta [Serialized.Text = true]) in type table [#"Y/N" = _t, #"#" = _t]),
-    #"Changed Type" = Table.TransformColumnTypes(Source,{{"Y/N", type text}, {"#", Int64.Type}})
-in
-    #"Changed Type"
