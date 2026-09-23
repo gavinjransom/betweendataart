e9bcd7bf516a4bab767049419cc868c3433d8117
Gavin Ransom <gavinjransom@gmail.com>
2026-09-23 19:35:01 +0100

    Minor changes, spacing etc

--- projects/lighthouse/js/lighthouse.js
+++ projects/lighthouse/js/lighthouse.js
@@ -19,92 +19,89 @@ function renderMap(svg, width, height) {
 
 // 3) Map Outline ----------------------------------------------------
 const path = d3.geoPath().projection(projection);
-
-  svg.selectAll('path')
-    .data(featureCollection.features)
-    .enter().append('path')
-    .attr('d', path)
-    .attr('fill', '#1d1b1b')
-    .attr('stroke', '#757e63')
-    .attr('stroke-width', 1);
+svg.selectAll('path')
+.data(featureCollection.features)
+.enter().append('path')
+.attr('d', path)
+.attr('class', 'map-outline')
+.attr('fill', '#1d1b1b')
+.attr('stroke', '#757e63')
+.attr('stroke-width', 1);
 
 // 4) Glow effect (SVG) ----------------------------------------------------
-  const defs = svg.append('defs');
+const defs = svg.append('defs');
 
-  const filter = defs.append('filter')
-    .attr('id', 'glow')
-    .attr('filterUnits', 'userSpaceOnUse')
-    .attr('x', '-50%')
-    .attr('y', '-50%')
-    .attr('width', '200%')
-    .attr('height', '200%');
+const filter = defs.append('filter')
+.attr('id', 'glow')
+.attr('filterUnits', 'userSpaceOnUse')
+.attr('x', '-50%')
+.attr('y', '-50%')
+.attr('width', '200%')
+.attr('height', '200%');
 
-  filter.append('feGaussianBlur')
-    .attr('stdDeviation', 10)
-    .attr('result', 'coloredBlur');
+filter.append('feGaussianBlur')
+.attr('stdDeviation', 10)
+.attr('result', 'coloredBlur');
 
-  const feMerge = filter.append('feMerge');
-  feMerge.append('feMergeNode')
-    .attr('in', 'coloredBlur');
-  feMerge.append('feMergeNode')
-    .attr('in', 'SourceGraphic');
+const feMerge = filter.append('feMerge');
+feMerge.append('feMergeNode')
+.attr('in', 'coloredBlur');
+feMerge.append('feMergeNode')
+.attr('in', 'SourceGraphic');
 
 // 5) Lighthouse Locations ------------------------------------------
-  svg.selectAll('circle')
-    .data(lighthouses)
-    .enter().append('circle')
-    .attr('cx', d => projection([+d.Longitude, +d.Latitude])[0])
-    .attr('cy', d => projection([+d.Longitude, +d.Latitude])[1])
-    .attr('r', 3)
-    .attr('fill', 'white')
-    .attr('stroke', 'grey')
-    .attr('stroke-width', 0.5)
-    .attr('filter', 'url(#glow)')
-    .attr('opacity', 0)
-    .transition()
-    .delay(300)
-    .duration(1250)
-    .attr('opacity', 1);
+svg.selectAll('circle')
+.data(lighthouses)
+.enter().append('circle')
+.attr('class', 'lighthouse-dot')
+.attr('cx', d => projection([+d.Longitude, +d.Latitude])[0])
+.attr('cy', d => projection([+d.Longitude, +d.Latitude])[1])
+.attr('r', 3)
+.attr('fill', 'white')
+.attr('stroke', 'grey')
+.attr('stroke-width', 0.5)
+.attr('filter', 'url(#glow)')
+.attr('opacity', 0)
+.transition()
+.delay(300)
+.duration(1250)
+.attr('opacity', 1);
 
 // 6) Tooltip -------------------------------------------------------
-  svg.selectAll(".hit-area")
-    .data(lighthouses)
-    .enter().append('circle')
-    .attr('cx', d => projection([+d.Longitude, +d.Latitude])[0])
-    .attr('cy', d => projection([+d.Longitude, +d.Latitude])[1])
-    .attr('r', 10)
-    .attr('fill', '#1d1b1b')
-    .attr('stroke', 'white')
-    .attr('stroke-width', 0.5)
-    .attr('opacity', 0)
-    .each(function (d) {
-      tippy(this, {
-        allowHTML: true,
-        content: `<b>${d.Name}</b><br>${d.Location}, ${d.Country}<br>Built: ${d.Year}`,
-        theme: 'light',
-        delay: [300, 200],
-        animation: 'fade',
-        duration: [1000, 500]
-      });
-    });
+svg.selectAll(".hit-area")
+.data(lighthouses)
+.enter().append('circle')
+.attr('cx', d => projection([+d.Longitude, +d.Latitude])[0])
+.attr('cy', d => projection([+d.Longitude, +d.Latitude])[1])
+.attr('r', 10)
+.attr('fill', '#1d1b1b')
+.attr('stroke', 'white')
+.attr('stroke-width', 0.5)
+.attr('opacity', 0)
+.each(function (d) {
+tippy(this, {
+allowHTML: true,
+content: `<b>${d.Name}</b><br>${d.Location}, ${d.Country}<br>Built: ${d.Year}`,
+theme: 'light',
+delay: [300, 200],
+animation: 'fade',
+duration: [1000, 500]
+});
+});
 
 // 7) Lighthouse 'blink effect' --------------------------------------
-  function blink() {
-    svg.selectAll('circle') // Target all lighthouse dots
-      .transition('blink')
-      .duration(10)
-      .attr('fill', '#1d1b1b')
-      .transition('blink')
-      .duration(10)
-      .attr('fill', 'white')
-      .end()
-      .then(() => {
-        setTimeout(blink, 6000);
-      });
-  }
+function blink() {
+svg.selectAll('circle') // Target all lighthouse dots
+.transition('blink')
+.duration(10)
+.attr('fill', '#1d1b1b')
+.transition('blink')
+.duration(10)
+.attr('fill', 'white')
+.end()
+.then(() => {setTimeout(blink, 6000);});}
 
-
-  blink();
+blink();
 }
 
 // 8) Render --------------------------------------------------------
